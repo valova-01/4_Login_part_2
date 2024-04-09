6988b11a61e81da04c4eae69b3c7f625235dbebe
anna <anna.valova.01@mail.ru>
2024-04-10 01:40:29 +0300

    login_work_2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-import { createElement } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import styles from './App.module.css';
+
+const schema = yup.object().shape({
+	email: yup
+		.string()
+		.matches(/\S+@\S+\.\S+/, 'Некорректный email')
+		.required('Email обязателен для заполнения'),
+	password: yup
+		.string()
+		.min(3, 'Пароль должен содержать минимум 3 символа')
+		.max(20, 'Пароль должен содержать максимум 20 символов')
+		.required('Пароль обязателен для заполнения'),
+	confirmPassword: yup
+		.string()
+		.test('password', 'Пароли не совпадают', function (value) {
+			return this.parent.password === value;
+		})
+		.required('Подтверждение пароля обязательно'),
+});
 
 export const App = () => {
-	const currentYear = new Date().getFullYear();
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement('p', null, 'Edit ', createElement('code', null, 'src/App.js'), ' and save to reload.'),
-			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-		),
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
 
-		createElement('footer', null, createElement('p', null, 'Текущий год: ', currentYear)),
+	const onSubmit = (dataForm) => {
+		console.log('Данные формы:', dataForm);
+	};
+
+	return (
+		<div className={styles.app}>
+			<h1>Регистрация</h1>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div>
+					<label>Email:</label>
+					<input type="email" {...register('email')} className={errors.email ? 'inputError' : ''} />
+					<span>{errors.email?.message}</span>
+				</div>
+				<div>
+					<label>Пароль:</label>
+					<input type="password" {...register('password')} className={errors.password ? 'inputError' : ''} />
+					<span>{errors.password?.message}</span>
+				</div>
+				<div>
+					<label>Подтвердите пароль:</label>
+					<input type="password" {...register('confirmPassword')} className={errors.confirmPassword ? 'inputError' : ''} />
+					<span>{errors.confirmPassword?.message}</span>
+				</div>
+				<button type="submit">Зарегистрироваться</button>
+			</form>
+		</div>
 	);
 };
